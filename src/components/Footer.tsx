4d8bae25d65d3cf376e774553cb10f77ba6a155b
IvanCastroRuiz <ivan.castro@devitech.com.co>
2024-11-12 22:11:11 -0500

    fix: probando Webhooks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white p-4 mt-8">
         <div className="container mx-auto text-center">
         <p>&copy; 2024 MegaShop. Todos los derechos reservados.</p>
+        <p>Ivan Jesus Castro Ruiz</p>
         </div>
     </footer>
   )
